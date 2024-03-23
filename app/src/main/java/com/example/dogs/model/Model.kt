7dcd4386d215f86ad7fbd4d9ba09a19112fec565
MB <cyrille.bebeytongo@stellantis.com>
2024-03-23 17:39:08 -0400

    setting up the model for using room db

--- app/src/main/java/com/example/dogs/model/Model.kt
+++ app/src/main/java/com/example/dogs/model/Model.kt
@@ -1,28 +1,42 @@
 package com.example.dogs.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity //for room library to treat this class as an entity that can be put in a DB/cache?
 data class DogBreed(
-    @SerializedName("id")
+    @ColumnInfo(name = "breed_id")
+    @SerializedName("id") //this must match what it is called in the API json
     val breedId: String?,
 
+    @ColumnInfo(name = "dog_name")
     @SerializedName("name")
     val dogBreed: String?,
 
+    @ColumnInfo(name = "life_span")
     @SerializedName("life_span")
     val lifespan: String?,
 
+    @ColumnInfo(name = "breed_group")
     @SerializedName("breed_group")
     val breedGroup: String?,
 
     // for retrofit to get data from json api for backend?
     //if var name matches name in JSON no need for annotation
+    @ColumnInfo(name = "breed_for")
     @SerializedName("bred_for")
     val bredFor: String?,
 
     @SerializedName("temperament")
     val temperament: String?,
 
+    @ColumnInfo(name = "dog_url")
     @SerializedName("url")
     val imageUrl: String?
-    )
+
+    ){ //data class doesn't need a body but we need a variable'
+    @PrimaryKey(autoGenerate = true)
+    var uuid: Int = 0
+}
