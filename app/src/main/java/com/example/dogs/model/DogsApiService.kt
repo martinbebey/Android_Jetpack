8252aa61f22ccd07e5c46993df61863e3ab3215c
MB <cyrille.bebeytongo@stellantis.com>
2024-03-22 21:16:20 -0400

    getting the remote data

--- app/src/main/java/com/example/dogs/model/DogsApiService.kt
+++ app/src/main/java/com/example/dogs/model/DogsApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class DogsApiService {
-    private val BASE_URL = "https://raw.githubusercontent.com/" //base url for the api json
+    private val BASE_URL = "https://raw.githubusercontent.com/DevTides/" //base url for the api json
     private val api = Retrofit.Builder()//builds a retrofit object
         .baseUrl(BASE_URL)//give it a base
         .addConverterFactory(GsonConverterFactory.create())//convert the json into the list of vars in our model
